817186f3f8c56b6f6ed55dae947ef4fc41f814d5
fischer8 <fischxr@hotmail.com>
2023-09-03 16:02:51 -0300

    dev

--- app/Components/Cards/Card.jsx
+++ app/Components/Cards/Card.jsx
@@ -6,11 +6,11 @@ import { BookmarkIcon as BookMarkOutline } from '@heroicons/react/24/outline'
 export default function Card(props) {
   const { card, handleFav } = props;
   return (
-    <section className="bg-black rounded-lg shadow-md p-4 relative" id={card.id}>
+    <section className="bg-white rounded-lg shadow-md p-4 relative" id={card.id}>
         {card.fav ?
-          <BookmarkIcon onClick={() => handleFav.del(card.id)} className="absolute right-3 top-2 hover:text-pink-100" fill='red' width={50} />
+          <BookmarkIcon onClick={() => handleFav(card.id, false)} className="absolute right-3 top-2 hover:text-pink-100" fill='red' width={50} />
           :
-          <BookMarkOutline onClick={() => handleFav.add(card.id)} className="absolute right-3 top-2 hover:text-pink-600" fill='grey' width={50} />
+          <BookMarkOutline onClick={() => handleFav(card.id, true)} className="absolute right-3 top-2 hover:text-pink-600" fill='grey' width={50} />
         }
         <Image src={card.img} alt={`Image ${card.id}`} height={200} width={200} className="w-full h-auto max-w-[200px]" />
         <button
--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@
 import { useEffect, useState, React } from 'react'
 import { Card, cardsData } from './Components';
 
-
 const Home = () => {
   const [films, setFilms] = useState(cardsData);
 
@@ -11,29 +10,22 @@ const Home = () => {
     setFilms(cards);
   }, [])
 
-  const add = (id) => {
+  const handleFavorites = (id, isFav) => {
     const cards = JSON.parse(localStorage.getItem("cards")) || cardsData;
-    cards[id].fav = true;
-    localStorage.setItem("cards", JSON.stringify(cards));
-    setFilms(cards)
-  }
-
-  const del = (id) => {
-    const cards = JSON.parse(localStorage.getItem("cards")) || cardsData;
-    cards[id].fav = false;
+    cards[id].fav = isFav;
     localStorage.setItem("cards", JSON.stringify(cards));
     setFilms(cards)
   }
 
   return (
-    <div>
+    <section>
       <h1 className="text-center text-6xl min-h-fit p-5">FiapFLix</h1>
-      <div className="min-h-screen flex flex-col justify-center items-center bg-gray-800">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          {films.map((film, i) => <Card key={i} card={film} handleFav={{ add, del }} />)}
-        </div>
-      </div>
-    </div>
+      <section className="min-h-screen flex flex-col justify-center items-center bg-gray-800">
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          {films.map((film, i) => <Card key={i} card={film} handleFav={handleFavorites} />)}
+        </section>
+      </section>
+    </section>
   );
 };
 
